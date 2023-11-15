1d495711dce664e9c8c4e5e9dcd2886e6ecb6771
Evaldo Junior <evaaldormj@gmail.com>
2023-11-15 01:03:57 -0300

    Feat: Adicionando contexto no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaUsuarios.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<UsuarioContext>(opt => opt.UseInMemoryDatabase("UsuarioLista"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
